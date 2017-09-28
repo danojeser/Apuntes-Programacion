9e546e90822d1c77f9e20e95f50bf1764a4e94f9
Daniel Ojeda <danojeser@gmail.com>
2017-09-28 12:52:48 +0200

    Ejercicios por terminar

--- Lenguaje_C/Capitulo_2/blo3ejer3.c
+++ Lenguaje_C/Capitulo_2/blo3ejer3.c
@@ -4,44 +4,63 @@
 
 int main (int argc, char *argv[]) {
 
-    int opcion, num1, num2, resultado;
+    int opcion;
+    float resultado, num1, num2;
 
-    printf("1. Cálculo de la hipotenusa de un triángulo.");
-    printf("2. Cálculo de la superficie de una circunferencia.");
-    printf("3. Cálculo del perímetro de una circunferencia.");
-    printf("4. Cálculo del área de un rectángulo.");
-    printf("5. Cálculo del área de un triángulo.");
-    printf("0. Salir de la aplicación.");
+    printf("1. Calculo de la hipotenusa de un triangulo.\n");
+    printf("2. Calculo de la superficie de una circunferencia.\n");
+    printf("3. Calculo del perimetro de una circunferencia.\n");
+    printf("4. Calculo del area de un rectangulo.\n");
+    printf("5. Calculo del area de un triangulo.\n");
+    printf("0. Salir de la aplicacion.\n");
     printf("Elige una opcion: ");
     scanf("%d",&opcion);
 
     switch (opcion) {
         case 1: //Calculo hipotenusa
             printf("Cateto 1: ");
-            scanf("%d",&num1);
+            scanf("%f",&num1);
 
             printf("Cateto 2: ");
-            scanf("%d",&num2);
+            scanf("%f",&num2);
             
-            resultado = pow((pow(num1, 2)+pow(num2,2)),(float)1/2);
-            printf ("\nResultado: %d\n", resultado);
+            //resultado = pow((pow(num1, 2)+pow(num2,2)),(float)1/2);
+            //printf ("\nResultado: %d\n", resultado);
             break;
         case 2: //Calculo superficie circunferencia
-            resultado = ;
-            printf ("\nResultado: %d\n", resultado);
+            
             break;
         case 3: 
-            resultado = num1 % num2;
-            printf ("\nResultado: %d\n", resultado);
+            printf("Dame el radio de la circunferencia: ");
+            scanf("%f",&num1);
+            resultado = (float)num1 * 2 * 3.1415;
+            printf ("\nLa circunferencia es de: %f", resultado);
             break;
         case 4:
+            printf("\nLa altura del rectangulo: ");
+            scanf("%f",&num1);
+
+            printf("\nLa base del rectangulo: ");
+            scanf("%f",&num2);
+
+            printf("El area del rectangulo es: %f",num1*num2);
             break;
         case 5:
+            printf("\nLa altura del triangulo: ");
+            scanf("%f",&num1);
+
+            printf("\nLa base del triangulo: ");
+            scanf("%f",&num2);
+
+            printf("El area del triangulo es: %f",(num1*num2)/2);
             break;
         default: 
             printf ("\nOpcion incorrecta\n");
             break;
     }
+
+    printf("\n");
+    system("pause");
     return 0;
     
 }
---
+++ Lenguaje_C/Capitulo_2/blo3ejer4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Escribe un programa que lea el importe de una compra y la cantidad recibida y calcule el cambio 
+a devolver, teniendo en cuenta que el número de monedas que se devuelven debe ser mínimo. 
+Suponer que el sistema monetario utilizado consta de monedas de 100, 50, 25, 5, y 1 unidad. 
+El precio viene dado en unidades monetarias enteras. 
+Ejemplo: Pagamos con 2000 unidades monetarias un articulo que nos cuesta 375 unidades monetarias, 
+la vuelta (1625 u) debería de ser de la siguiente forma: 
+- 16 monedas de 100 = 1600 
+- 1 moneda de 25 = 25*/
+
+int main (int argc, char *argv[]) {
+    
+    
+}
