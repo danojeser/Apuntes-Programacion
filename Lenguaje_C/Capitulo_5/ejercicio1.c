0765d3712ebd82ff7428431e11a8d2eabf45b31b
Daniel Ojeda <danojeser@gmail.com>
2017-10-18 14:42:23 +0200

    Cambios clase

---
+++ Lenguaje_C/Capitulo_5/ejercicio1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main (int argc, char *argv[]) {
+
+	int a, b, c;
+	printf("Primer numero: ");
+    scanf("%d", &a);
+
+    printf("\n");
+    system("pause");
+    return 0;
+    
+}
