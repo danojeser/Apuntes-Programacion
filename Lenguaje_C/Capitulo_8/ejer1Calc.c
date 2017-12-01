05fffe8e30a4e179842dceffbb3d34f7d77984ad
Daniel Ojeda <danojeser@gmail.com>
2017-12-01 14:58:04 +0100

    Calculadora punteros

--- Lenguaje_C/Capitulo_8/ejer1Calc.c
+++ Lenguaje_C/Capitulo_8/ejer1Calc.c
@@ -10,45 +10,45 @@ void division(int *var1, int *var2, int *resultado);
 
 int main (int argc, char *argv[]) {
 
-	// Declaración de variables
+    // Declaración de variables
     int num1, num2, resultado, opcion;
-    
-   // Impresión del menú.
-   opcion = mostrarMenu ();
+
+    // Impresión del menú.
+    opcion = mostrarMenu ();
    
-   // Lectura de los dos números necesarios para cualquier operación
-   if ((opcion > 0) && (opcion < 5)) {
-          printf ("\n\nIntroduzca el primer número: ");
-          scanf ("%d", &num1);
-          printf ("\nIntroduzca el segundo número: ");
-          scanf ("%d", &num2);
-   }
+    // Lectura de los dos números necesarios para cualquier operación
+    if ((opcion > 0) && (opcion < 5)) {
+        printf ("\n\nIntroduzca el primer número: ");
+        scanf ("%d", &num1);
+        printf ("\nIntroduzca el segundo número: ");
+        scanf ("%d", &num2);
+    }
    
    // switch para conseguir la operación
-   switch (opcion) {
-          case 0: // Salimos del programa sin realizar acción alguna
-               printf ("\nADIOS");
-               break;
-          case 1: // Se selecciona una suma
-               suma(&num1, &num2, &resultado);
-               printf ("\nResultado: %d\n", resultado);
-               break;
-          case 2: // Se selecciona una resta
-               resta(&num1, &num2, &resultado);
-               printf ("\nResultado: %d\n", resultado);
-               break;
-          case 3: // Se selecciona una multiplicación
-               multiplicacion(&num1, &num2, &resultado);
-               printf ("\nResultado: %d\n", resultado);
-               break;
-          case 4: // Se selecciona una división
-               division(&num1, &num2, &resultado);
-               printf ("\nResultado: %d\n", resultado);
-               break;
-          default: // Opción incorrecta en el menú
-               printf("\nOpción incorrecta\n");
-               break;
-   }
+    switch (opcion) {
+            case 0: // Salimos del programa sin realizar acción alguna
+                printf ("\nADIOS");
+                break;
+            case 1: // Se selecciona una suma
+                suma(&num1, &num2, &resultado);
+                printf ("\nResultado: %d\n", resultado);
+                break;
+            case 2: // Se selecciona una resta
+                resta(&num1, &num2, &resultado);
+                printf ("\nResultado: %d\n", resultado);
+                break;
+            case 3: // Se selecciona una multiplicación
+                multiplicacion(&num1, &num2, &resultado);
+                printf ("\nResultado: %d\n", resultado);
+                break;
+            case 4: // Se selecciona una división
+                division(&num1, &num2, &resultado);
+                printf ("\nResultado: %d\n", resultado);
+                break;
+            default: // Opción incorrecta en el menú
+                printf("\nOpción incorrecta\n");
+                break;
+    }
 
     printf("\n");
     system("pause");
@@ -59,23 +59,24 @@ int main (int argc, char *argv[]) {
 
 int mostrarMenu () {
     int opcion;
-   // Impresión del menú. 
-   printf ("\nMENU DE CALCULADORA");
-   printf ("\n\t1.- Suma");
-   printf ("\n\t2.- Resta");
-   printf ("\n\t3.- Multiplicación");
-   printf ("\n\t4.- División");
-   printf ("\n\n\t0.- Salir");
-   printf ("\n\n\tIntroduzca una opción: ");
-   // Lectura de la opción elegida por el usuario
-   scanf ("%d", &opcion);
-   return opcion;
+    // Impresión del menú. 
+    printf ("\nMENU DE CALCULADORA");
+    printf ("\n\t1.- Suma");
+    printf ("\n\t2.- Resta");
+    printf ("\n\t3.- Multiplicación");
+    printf ("\n\t4.- División");
+    printf ("\n\n\t0.- Salir");
+    printf ("\n\n\tIntroduzca una opción: ");
+    // Lectura de la opción elegida por el usuario
+    scanf ("%d", &opcion);
+    return opcion;
 }
 
 
 void suma(int *var1, int *var2, int *resultado){
     *(resultado) = *(var1) + *(var2);
 }
+
 
 void resta(int *var1, int *var2, int *resultado){
     *(resultado) = *(var1) - *(var2);
